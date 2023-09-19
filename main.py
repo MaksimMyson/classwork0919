cb4dcf8fbece814316dedebdf8bd0da7d4a9bf69
IT STEP <memasik4you@gmal.com>
2023-09-19 18:56:43 +0300

    ex1

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-
+a = int(input())
+if a == 1:
+    print("Понеділок")
+elif a == 2:
+    print("Вівторок")
+elif a == 3:
+    print("Середа")
+elif a == 4:
+    print("Четвер")
+elif a == 5:
+    print("Пятниця")
+elif a == 6:
+    print("Субота")
+elif a == 7:
+    print("Неділя")
